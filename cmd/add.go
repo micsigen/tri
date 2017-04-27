1d918d8750883d1e4c7f4deac807f8ae4cfa679e
bmajor <major.balazs@icloud.com>
2017-04-27 15:15:55 +0200

    print item after add command

--- cmd/add.go
+++ cmd/add.go
@@ -31,8 +31,9 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		// TODO: Work your own magic here
-		fmt.Println("add called")
+		for _, x := range args {
+			fmt.Println(x)
+		}
 	},
 }
 
